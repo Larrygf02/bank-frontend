0897452c16e1508205a5bfeb3e1253e28c9ed3b7
larrygf02 <raulgf_02@hotmail.com>
2019-12-30 22:02:53 -0500

    get states formulario persona

--- src/componentes/persons/NewPerson.js
+++ src/componentes/persons/NewPerson.js
@@ -1,66 +1,77 @@
 import React, { Component, useState } from 'react';
 import { Link } from 'react-router-dom'
 
-class NewPerson extends Component {
-    render() {
-        // state
-        const [ name, saveName ] = useState('')
-        const [ surname, saveSurname ] = useState('')
-        const [ dni, saveDNI ] = useState('')
-        const [ date, saveDate] = useState('')
 
-        return (
-            <div className="row">
-                <div className="col-12 mb-4">
-                    <Link to={'/personas'} className="btn btn-secondary">
-                        <i className="fas fa-arrow-circle-left"></i> {''}
-                        Volver al listado
-                    </Link>
-                </div>
-                <div className="col-12">
-                    <h2>
-                        <i className="fas fa-user"></i> {''}
-                        Nueva Persona
-                    </h2>
-                    <div className="row justify content-center">
-                        <div className="col-md-8 mt-5">
-                            <form>
-                                <div className="form-group">
-                                    <label>Nombre: </label>
-                                    <input 
-                                        type="text"
-                                        className="form-control"
-                                        placeholder="Nombre"/>
-                                </div>
-                                <div className="form-group">
-                                    <label>Apellido: </label>
-                                    <input 
-                                        type="text"
-                                        className="form-control"
-                                        placeholder="Apellido"/>
-                                </div>
-                                <div className="form-group">
-                                    <label>DNI: </label>
-                                    <input 
-                                        type="text"
-                                        className="form-control"
-                                        placeholder="DNI"/>
-                                </div>
-                                <div className="form-group">
-                                    <label>Fecha Nacimiento: </label>
-                                    <input 
-                                        type="text"
-                                        className="form-control"
-                                        placeholder="Fecha Nacimiento"/>
-                                </div>
-                                <input type="submit" value="Agregar persona" className="btn btn-success"/>
-                            </form>
-                        </div>
+const NewPerson = ({history}) =>  {
+    // state
+    const [ name, saveName ] = useState('')
+    const [ surname, saveSurname ] = useState('')
+    const [ dni, saveDNI ] = useState('')
+    const [ date, saveDate] = useState('')
+
+    // Agregar una nueva persona
+    const submitNewPerson = e => {
+        e.preventDefault();
+        
+    }
+
+    return (
+        <div className="row">
+            <div className="col-12 mb-4">
+                <Link to={'/personas'} className="btn btn-secondary">
+                    <i className="fas fa-arrow-circle-left"></i> {''}
+                    Volver al listado
+                </Link>
+            </div>
+            <div className="col-12">
+                <h2>
+                    <i className="fas fa-user"></i> {''}
+                    Nueva Persona
+                </h2>
+                <div className="row justify content-center">
+                    <div className="col-md-8 mt-5">
+                        <form onSubmit={submitNewPerson}>
+                            <div className="form-group">
+                                <label>Nombre: </label>
+                                <input 
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Nombre"
+                                    value={name}
+                                    onChange={ e => saveName(e.target.value)}/>
+                            </div>
+                            <div className="form-group">
+                                <label>Apellido: </label>
+                                <input 
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Apellido"
+                                    value={surname}
+                                    onChange={ e => saveSurname(e.target.value)}/>
+                            </div>
+                            <div className="form-group">
+                                <label>DNI: </label>
+                                <input 
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="DNI"
+                                    value={dni}
+                                    onChange={ e => saveDNI(e.target.value)}/>
+                            </div>
+                            <div className="form-group">
+                                <label>Fecha Nacimiento: </label>
+                                <input 
+                                    type="date"
+                                    className="form-control"
+                                    placeholder="Fecha Nacimiento"/>
+                            </div>
+                            <input type="submit" value="Agregar persona" className="btn btn-success"/>
+                        </form>
                     </div>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default NewPerson;
